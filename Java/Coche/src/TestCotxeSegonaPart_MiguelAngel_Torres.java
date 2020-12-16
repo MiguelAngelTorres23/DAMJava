21accafbd5cb587755bb78db35af9aefeb5f1827
Molomatf <47225549+Molomatf@users.noreply.github.com>
2020-12-16 15:21:03 +0100

    Casi Listo

--- Java/Coche/src/TestCotxeSegonaPart_MiguelAngel_Torres.java
+++ Java/Coche/src/TestCotxeSegonaPart_MiguelAngel_Torres.java
@@ -9,9 +9,13 @@
 public class TestCotxeSegonaPart_MiguelAngel_Torres {
     public static void main(String[] args) {
         CotxeSegonaPart_MiguelAngel_Torres c1 = new CotxeSegonaPart_MiguelAngel_Torres("Kia", "1", TipusCanvi.CanviAutomatic, EstatsMotorCotxe.Aturat,CanviarMarxaAutomatic.N, null);
+        CotxeSegonaPart_MiguelAngel_Torres c2 = new CotxeSegonaPart_MiguelAngel_Torres("Kia", "1", TipusCanvi.CanviManual, EstatsMotorCotxe.Aturat,null, CanviarMarxaManual.N);
 
         c1.CanviarMarxaAutomatic('+');
-        System.out.println("La marcha actual es: " + c1.getCanviarmarxaautomatic());
+        System.out.println("La marcha actual de c1 es: " + c1.getCanviarmarxaautomatic());
+
+        c2.CanviarMarxaManual('-');
+        System.out.println("La marcha actual de c2 es: " + c2.getCanviarmarxamanual());
 
 
     }
